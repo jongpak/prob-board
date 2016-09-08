1c86de1e8411d299779170f6b48de74d18ce3667
jongpak <jplabs@canxan.com>
2016-09-08 21:07:06 +0900

    Add Board entity

---
+++ app/Entity/Board.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * @Entity
+ * @Table(name="boards")
+ */
+class Board
+{
+
+
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @Column(type="text", length=255)
+     */
+    protected $name;
+
+    /**
+     * @Column(type="text", length=255)
+     */
+    protected $title;
+}
