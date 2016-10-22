7dc3217fe6a1712de744534f0c14f5ae728c176c
jongpak <jplabs@canxan.com>
2016-10-22 12:43:32 +0900

    FormValidator 설정파일의 잘못된 컨트롤러 검사하도록 함

--- app/Bootstrap/FormValidatorBootstrap.php
+++ app/Bootstrap/FormValidatorBootstrap.php
@@ -4,11 +4,22 @@ namespace App\Bootstrap;
 
 use App\EventListener\FormValidator;
 use Core\Bootstrap\BootstrapInterface;
+use Prob\Handler\ProcFactory;
 
 class FormValidatorBootstrap implements BootstrapInterface
 {
     public function boot(array $env)
     {
+        $this->validateController($env['formValidation'], $env['router']['namespace']);
+
         FormValidator::setRule($env['formValidation']);
+    }
+
+    public function validateController(array $rule, $namespace)
+    {
+        $keys = array_keys($rule);
+        foreach($keys as $key) {
+            ProcFactory::getProc($namespace . '\\' . $key);
+        }
     }
 }
