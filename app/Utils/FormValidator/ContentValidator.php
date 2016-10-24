8f9de699a1f5795654319a336337be0237abf096
jongpak <jplabs@canxan.com>
2016-10-25 00:32:51 +0900

    Used strict type equal operator in validators

--- app/Utils/FormValidator/ContentValidator.php
+++ app/Utils/FormValidator/ContentValidator.php
@@ -9,14 +9,14 @@ class ContentValidator
 {
     public static function subjectValidate($value)
     {
-        if(Validator::stringType()->length(1, 255)->validate($value) == false) {
+        if(Validator::stringType()->length(1, 255)->validate($value) === false) {
             throw new InvalidRequestException("Length of subject is 1 ~ 255");
         }
     }
 
     public static function contentValidate($value)
     {
-        if(Validator::stringType()->length(1, 65535)->validate($value) == false) {
+        if(Validator::stringType()->length(1, 65535)->validate($value) === false) {
             throw new InvalidRequestException("Length of content is 1 ~ 65535");
         }
     }
