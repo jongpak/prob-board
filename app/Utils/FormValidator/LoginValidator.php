443557dc2b005e9e6fd82539f7501a1a753c4b54
jongpak <jplabs@canxan.com>
2016-10-26 00:44:03 +0900

    Fixed a double quote to single quotes

--- app/Utils/FormValidator/LoginValidator.php
+++ app/Utils/FormValidator/LoginValidator.php
@@ -10,14 +10,14 @@ class LoginValidator
     public static function accountIdValidate($value)
     {
         if(Validator::stringType()->length(4, 128)->alnum()->validate($value) === false) {
-            throw new InvalidRequestException("Length of account id is 4 ~ 128");
+            throw new InvalidRequestException('Length of account id is 4 ~ 128');
         }
     }
 
     public static function passwordValidate($value)
     {
         if(Validator::stringType()->length(4, 128)->validate($value) === false) {
-            throw new InvalidRequestException("Length of account password is 4 ~ 128");
+            throw new InvalidRequestException('Length of account password is 4 ~ 128');
         }
     }
 }
