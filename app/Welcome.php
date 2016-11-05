9c9e64d0b252ce9297bdf0c0c2d279bb24178143
jongpak <jplabs@canxan.com>
2016-11-06 00:30:27 +0900

    Fixed error of conflict namespace Controller::Board and Entity::Board

--- app/Welcome.php
+++ app/Welcome.php
@@ -4,13 +4,13 @@ namespace App\Controller;
 
 use Core\Utils\EntityUtils\EntitySelect;
 use Core\ViewModel;
-use App\Entity\Board;
+use App\Entity\Board as BoardModel;
 
 class Welcome
 {
     public function index(ViewModel $viewModel)
     {
-        $viewModel->set('boards', EntitySelect::select(Board::class)->findAll());
+        $viewModel->set('boards', EntitySelect::select(BoardModel::class)->findAll());
         return 'welcome';
     }
 }
